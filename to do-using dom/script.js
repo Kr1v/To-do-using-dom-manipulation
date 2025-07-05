6685155607e1b5f7b0eab1314034741d18e8671b
Kritika Mehta <162875055+Kr1v@users.noreply.github.com>
2025-07-05 12:02:55 +0530

    Update script.js

--- to do-using dom/script.js
+++ to do-using dom/script.js
@@ -14,3 +14,24 @@ function typewriter(){
 
 }
     window.onload=typewriter;
+
+function createleave(count){
+    for(let i=0;i<count;i++){
+       const container=document.getElementById("leafy");
+       if(!container)return; 
+       const leaf=document.createElement('span');
+    
+       leaf.classList.add('leaves');
+       leaf.style.left=Math.random()*100+'vw';
+       leaf.style.animationDuration=5+Math.random()*5+'s';
+       leaf.style.animationDelay=Math.random()*5+'s';
+       document.getElementById("leafy").appendChild(leaf);
+       console.log("works?");
+       setTimeout(()=>{
+        leaf.remove();
+    },10000);
+    }   
+    
+}
+setInterval(()=>{createleave(5)},300);
+
